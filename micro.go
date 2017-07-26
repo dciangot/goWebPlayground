aa3252aad5bfca07c358d08e30a60cbc7c70625c
dciangot <diego.ciangottini@cern.ch>
2017-07-26 11:47:14 +0200

    Add request handler to micro

--- micro.go
+++ micro.go
@@ -1,13 +1,59 @@
 package main
 
 import (
+	"crypto/tls"
+	"flag"
 	"fmt"
 	"net/http"
 	"io/ioutil"
 	"path/filepath"
 	"os"
+	"log"
 	//"encoding/json"
 )
+
+var (
+	certFile = flag.String("cert", "usercert.pem", "A PEM eoncoded certificate file.")
+	keyFile  = flag.String("key", "unencrypted.pem", "A PEM encoded private key file.")
+)
+
+func requestHandler(url string, uri string, verb string, cert string, key string) error{
+
+	// Load client cert
+	certificate, err := tls.LoadX509KeyPair(cert, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{certificate},
+        InsecureSkipVerify: true,
+	}
+
+    tlsConfig.BuildNameToCertificate()
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{Transport: transport}
+
+    req, _ := http.NewRequest(verb, url + uri, nil)
+
+	log.Println("start")
+    req.Header.Set("Accept", "application/json")
+    resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+        return err
+	}
+	defer resp.Body.Close()
+
+	// Dump response
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+        return err
+	}
+	log.Println(string(data))
+    return nil
+}
 
 func listUsers(response http.ResponseWriter, req *http.Request) {
 	//fmt.Fprintf(response, "Welcome, %s!", req.URL.Path)
@@ -16,6 +62,13 @@ func listUsers(response http.ResponseWriter, req *http.Request) {
 	//js, _ := json.Marshal(data)
 	fmt.Printf("%s", currentDir)
 	fmt.Fprintf(response, "%s" , data)
+
+	flag.Parse()
+    requestHandler("https://cmsweb-testbed.cern.ch/crabserver/preprod/filetransfers",
+				   "?subresource=acquiredTransfers&asoworker=asodciangot1&grouping=0", 
+				   "GET", *certFile, *keyFile)
+}
+
 }
 
 func main() {
